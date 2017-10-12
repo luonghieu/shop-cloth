6b8e399439d6372948c598aaa8dcab9869829dd2
luonghieu <luonghieu.t3@gmail.com>
2017-10-13 01:10:44 +0700

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
 			'name' =>'sql9199098',
 			'username'=>'sql9199098',
 			'password'=>'JdKs7yKrlp',
-			'connection'=>'sql9.freemysqlhosting.net',
+			'connection'=>'mysql:host=sql9.freemysqlhosting.net',
 			'options'=>[
 				PDO::ATTR_ERRMODE =>PDO::ERRMODE_EXCEPTION
 			]
